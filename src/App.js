774971e0c2538548d63d32284d4e0bf2980881cf
webdev689 <kumar.lori@yahoo.in>
2017-11-26 20:29:06 -0500

    clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ListNamesContainer from './components/containers/ListNamesContainer';
-import AddListContainer from './components/containers/AddListContainer';
-import AddCategoryContainer from './components/containers/AddCategoryContainer';
-import CurrentListContainer from './components/containers/CurrentListContainer';
 import AsidePresentation from './components/presentation/pageLayout/AsidePresentation';
 import SectionPresentation from './components/presentation/pageLayout/SectionPresentation';
 
 
 
-/*const App = () => (
-    <main className="main">
-        <ListNamesContainer/>
-        <CurrentListContainer />
-    </main>
-);*/
 
 const App = () => (
 
@@ -29,37 +18,6 @@ const App = () => (
 export default App;
 
 
-
-
-
-/*<div className="App">
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-</div>*/
-
-
-
-/*class App extends Component {
-
-  render() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-        </div>
-    );
-  }
-}*/
 
 
 
